e7b68a6946e4d35dd49c6a527b249ff632311199
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2023-02-25 14:52:35 +0900

    Fix group names

--- holodex/__main__.py
+++ holodex/__main__.py
@@ -9,8 +9,16 @@ def get_args():
 
 def run():
     args = get_args()
+    group = args.group
+    if args.group.title() == "Hololive" or args.group.title() == "Nijisanji" or args.group.title() == "Independents":
+        group =  args.group.title()
+    elif args.group.upper() == "VOMS":
+        group = args.group.upper()
+    elif args.group.title() == "Indie":
+        group = "Independents"
+    
 
-    ListStreams.run(org=args.group)
+    ListStreams.run(org=group)
 
 
 if __name__ == "__main__":
